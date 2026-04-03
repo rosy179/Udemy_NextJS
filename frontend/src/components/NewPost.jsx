e78532f437b546fc2bff1a1d380f78985475f5f6
rosy179 <lerosy179@gmail.com>
2026-04-03 10:42:02 +0700

    chinh lai layout form create new post

--- frontend/src/components/NewPost.jsx
+++ frontend/src/components/NewPost.jsx
@@ -1,15 +1,25 @@
 import classes from "./NewPost.module.css";
 
-function NewPost() {
+function NewPost(props) {
   return (
     <form className={classes.form}>
       <p>
         <label htmlFor="author">Author</label>
-        <textarea id="author" required rows="3"></textarea>
+        <textarea
+          id="author"
+          required
+          rows="3"
+          onChange={props.onChangeAuthor}
+        ></textarea>
       </p>
       <p>
         <label htmlFor="content">Content</label>
-        <textarea id="content" required rows="5"></textarea>
+        <textarea
+          id="content"
+          required
+          rows="5"
+          onChange={props.onChangeContent}
+        ></textarea>
       </p>
     </form>
   );
