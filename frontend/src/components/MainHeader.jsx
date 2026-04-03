02e7650b590867110c07628031f1cd18b40143d8
rosy179 <lerosy179@gmail.com>
2026-04-03 10:41:00 +0700

    them layout main header chua title va button create post

---
+++ frontend/src/components/MainHeader.jsx
@@ -0,0 +1,20 @@
+import classes from "./MainHeader.module.css";
+import { MdPostAdd, MdMessage } from "react-icons/md";
+
+function MainHeader({ onCreatePost }) {
+  return (
+    <header className={classes.header}>
+      <h1 className={classes.logo}>
+        <MdMessage />
+        React Poster
+      </h1>
+      <p>
+        <button className={classes.button} onClick={onCreatePost}>
+          <MdPostAdd size={18} /> Create Post
+        </button>
+      </p>
+    </header>
+  );
+}
+
+export default MainHeader;
